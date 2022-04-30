5d278dbf0cb18d6be43bce89ce73c2abcbe93917
Wayne Theisinger <wayne@mowdirect.co.uk>
2022-04-30 07:38:34 +0000

    build: comment stub

--- build/phar/_cli_stub.php
+++ build/phar/_cli_stub.php
@@ -4,7 +4,7 @@
 Phar::mapPhar('itjonction-img-opt.phar');
 
 $application = require 'phar://itjonction-img-opt.phar/src/bootstrap.php';
-$application->setPharMode(true);
-$application->run();
+//$application->setPharMode(true);
+//$application->run();
 
 __HALT_COMPILER();
